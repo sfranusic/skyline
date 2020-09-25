4c70013bf349d84eb0d92b5e5956aa102cb58a2e
Samuel Franusic <sfranusic@gmail.com>
2020-09-25 11:00:11 -0700

    Set up initial project

---
+++ skyline/skyline/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  skyline
+//
+//  Created by Sammy Franusic on 9/25/20.
+//  Copyright © 2020 sf. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ skyline/skylineTests/skylineTests.swift
@@ -0,0 +1,34 @@
+//
+//  skylineTests.swift
+//  skylineTests
+//
+//  Created by Sammy Franusic on 9/25/20.
+//  Copyright © 2020 sf. All rights reserved.
+//
+
+import XCTest
+@testable import skyline
+
+class skylineTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
